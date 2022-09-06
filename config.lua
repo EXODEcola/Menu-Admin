df764822595fcb177317a1619803557c6c52d2a6
EXODE cola <87906167+EXODEcola@users.noreply.github.com>
2022-09-06 22:01:08 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+Config = {
+    webhook = { -- LOGS DISCORD
+        Staffmodeon = "",      -- MODE STAFF ON
+        Staffmodeoff = "",     -- MODE STAFF OFF
+        teleport = "",
+        teleportTo = "",
+        revive = "",
+        kick = "",
+        SendLogs = "",        -- GENERAL LOGS
+        report = "",
+        givemoney = ""
+    },
+
+    Touche = { -- TOUCHE MENU
+        Noclip = 170, --F3
+        Menu = 57, --F10
+        MenuReport = 344, --F11
+    }
+}
